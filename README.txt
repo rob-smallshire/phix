af46684cb02f674475ac6931d008109e63e72e99
robert <robert@smallshire.org.uk>
2011-07-07 12:42:28 +0200

    Initial configuration

---
+++ README.txt
@@ -0,0 +1,6 @@
+Phix is a Sphinx extension which allows you to reference particular ArgoUML
+diagrams in a *.zargo file directly in the ReStructuredText source of Sphinx
+documentation. Phix creates SVG diagrams from the ArgoUML source and embeds them
+in the Sphinx output.
+
+Phix was the sphinx in Greek mythology.
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.txt', 'r') as readme:
+    long_description = readme.read()
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='phix',
+    packages=['phix'],
+    version='0.5',
+    url='http://code.google.com/p/phix/',
+    download_url='http://pypi.python.org/pypi/phix',
+    license='BSD',
+    author='Robert Smallshire',
+    author_email='robert@smallshire.org.uk',
+    description='Sphinx "phix" extension',
+    long_description=long_description,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    #packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+)
