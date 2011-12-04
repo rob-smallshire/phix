f7dc3f388d55ccf64563ee7e365d6838fe5922cf
Austin Bingham <austin.bingham@gmail.com>
2011-12-04 09:07:28 +0100

    integrated new argouml test, and refactored test harness.

--- phix/test/phix_tests.py
+++ phix/test/phix_tests.py
@@ -10,23 +10,48 @@ import phix.websequencediagram
 
 test_dir = os.path.split(__file__)[0]
 
+def build_project(project_name):
+    '''Build and clean a sphix project in a given directory.
+
+    This will throw an exception if either the build or clean of the
+    project fails.
+
+    Args:
+      * project_name: The name of the directory containing the project
+        to build.
+    '''
+    project_dir = os.path.join(test_dir, project_name)
+
+    # The commands that will be executed, in order, in the project
+    # directory.
+    commands = [
+        'make html',
+        'make clean'
+        ]
+
+    # Save stdout in "<project_name>.stdout"
+    with open('{}.stdout'.format(project_name), 'w') as stdout:
+        # Save stderr in "<project_name>.stderr"
+        with open('{}.stderr'.format(project_name), 'w') as stderr:
+
+            # Run each command in turn.
+            for command in commands:
+                subprocess.check_call(
+                    command.split(),
+                    cwd=project_dir,
+                    stdout=stdout,
+                    stderr=stderr)
+
 class Tests(unittest.TestCase):
     def test_dia(self):
-        project_dir = os.path.join(test_dir, 'dia_project')
+        '''Build a project using the dia extension.
+        '''
+        build_project('dia_project')
 
-        commands = [
-            'make html',
-            'make clean'
-            ]
-
-        with open('dia_project.stdout', 'w') as stdout:
-            with open('dia_project.stderr', 'w') as stderr:
-                for command in commands:
-                    subprocess.check_call(
-                        command.split(),
-                        cwd=project_dir,
-                        stdout=stdout,
-                        stderr=stderr)
+    def test_argouml(self):
+        '''Build a project using the argouml extension.
+        '''
+        build_project('argouml_project')
 
 if __name__ == '__main__':
     unittest.main()
