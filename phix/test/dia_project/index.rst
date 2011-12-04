bcbda1fdb4a5e33621836b8214396929b1261f63
Austin Bingham <austin.bingham@gmail.com>
2011-12-04 08:54:32 +0100

    added a dia test.

---
+++ phix/test/dia_project/index.rst
@@ -0,0 +1,19 @@
+.. phix test project for dia documentation master file, created by
+   sphinx-quickstart on Sun Dec  4 08:27:19 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to phix test project for dia's documentation!
+=====================================================
+
+.. dia:: test.dia
+   :new-window:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- phix/test/phix_tests.py
+++ phix/test/phix_tests.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import unittest
 
 import phix.argouml
@@ -6,6 +8,26 @@ import phix.inkscape
 import phix.websequencediagram
 
 
+test_dir = os.path.split(__file__)[0]
+
 class Tests(unittest.TestCase):
-    pass
+    def test_dia(self):
+        project_dir = os.path.join(test_dir, 'dia_project')
+
+        commands = [
+            'make html',
+            'make clean'
+            ]
+
+        with open('dia_project.stdout', 'w') as stdout:
+            with open('dia_project.stderr', 'w') as stderr:
+                for command in commands:
+                    subprocess.check_call(
+                        command.split(),
+                        cwd=project_dir,
+                        stdout=stdout,
+                        stderr=stderr)
+
+if __name__ == '__main__':
+    unittest.main()
 
